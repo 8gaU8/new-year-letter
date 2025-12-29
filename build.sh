39038f355dc3656d130f7b74391ee893d746713f
8gaU8 <71952256+8gaU8@users.noreply.github.com>
2025-12-29 21:39:58 +0900

    update example

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ set -euxo pipefail
 cd "$(dirname "$0")"
 
 # Generate address.tex
-python3 scripts/main.py ./scripts/data/address.csv > address.tex
+python3 scripts/main.py ./scripts/data/example.address.csv > address.tex
 docker compose up
 
